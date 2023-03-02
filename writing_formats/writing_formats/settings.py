358af5595c6a1fb20a50f1c11557a1ed0f343810
keynes okiro <okiroochieng@gmail.com>
2023-03-02 13:42:53 +0300

    Adding db setup

--- writing_formats/writing_formats/settings.py
+++ writing_formats/writing_formats/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'writing_formats.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mla_papers',
+        'USER': 'postgres',
+        'PASSWORD': '0000',
+        'HOST': 'localhost'
     }
 }
 
