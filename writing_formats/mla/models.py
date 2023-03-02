bb2d02c7c2577075a8ee1ea7697e8b638b6ddcc3
keynes okiro <okiroochieng@gmail.com>
2023-03-02 14:38:16 +0300

    -primary key field attribute added

--- writing_formats/mla/models.py
+++ writing_formats/mla/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class title_page(models.Model):
+    paper_id = models.IntegerField()
     professor_name= models.CharField(max_length=30)
     course_id= models.CharField(max_length=70)
     date= models.CharField(max_length=20)
