c5513c2589212fe6f433c743fb7da8fd7d5d48ca
keynes okiro <okiroochieng@gmail.com>
2023-03-02 13:56:20 +0300

    changed : to = in  mla app models

--- writing_formats/mla/models.py
+++ writing_formats/mla/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class title_page(models.Model):
-    paper_id: models.IntegerField()
-    professor_name: models.CharField(max_length=30)
-    course_id: models.CharField(max_length=70)
-    date: models.CharField(max_length=20)
-    paper_title: models.CharField(max_length=50)
+    paper_id= models.IntegerField()
+    professor_name= models.CharField(max_length=30)
+    course_id= models.CharField(max_length=70)
+    date= models.CharField(max_length=20)
+    paper_title= models.CharField(max_length=50)
