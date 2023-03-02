9dad44239548da7b077c33225c63895ad6564d08
keynes okiro <okiroochieng@gmail.com>
2023-03-02 13:53:52 +0300

    creating mla app models

--- writing_formats/mla/models.py
+++ writing_formats/mla/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class title_page(models.Model):
+    paper_id: models.IntegerField()
+    professor_name: models.CharField(max_length=30)
+    course_id: models.CharField(max_length=70)
+    date: models.CharField(max_length=20)
+    paper_title: models.CharField(max_length=50)
